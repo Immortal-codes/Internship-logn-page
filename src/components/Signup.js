785ffa77914d124ec1a5f5916b68c9e8e8271511
Rajan Sharma <rajanraj8979@gmail.com>
2024-02-23 09:49:23 +0530

    first commit

---
+++ src/components/Signup.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./Signup.css";
+
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData); // You can perform further actions here, such as sending the data to a server
+  };
+
+  return (
+    <div className="container">
+      <h1 style={{ marginLeft: "80px" }}> User Registeration </h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Enter Username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter Email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter Password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
